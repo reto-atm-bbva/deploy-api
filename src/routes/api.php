0c577002daf9da20dcdaf2198a6b5da56c066fb1
Alexander Ramos <tcristian_15.go@hotmail.com>
2022-10-23 11:21:20 -0500

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
 
 
 //Route::get('data', [\App\Http\Controllers\WelcomeController::class, 'getData']);
